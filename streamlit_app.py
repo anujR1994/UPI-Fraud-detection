e6c8c3b7393856f77168cf82b0757db98c6aa92d
shourya-goenka <54571965+shourya-goenka@users.noreply.github.com>
2024-05-12 19:55:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,25 +13,25 @@ You have the option of inspecting a single transaction by adjusting the paramete
 multiple transactions at once by uploading a .csv file in the specified format
 """
 # Function to validate time format
-def validate_time_format(input_time):
-    pattern = re.compile(r'^([0-1]?[0-9]|2[0-3]):([0-5]?[0-9]):([0-5]?[0-9])$')
-    if pattern.match(input_time):
-        return True
-    else:
-        return False
+# def validate_time_format(input_time):
+#     pattern = re.compile(r'^([0-1]?[0-9]|2[0-3]):([0-5]?[0-9]):([0-5]?[0-9])$')
+#     if pattern.match(input_time):
+#         return True
+#     else:
+#         return False
 
-tran_date = st.date_input("Select the date of your transaction", datetime.date.today())
-tran_time = st.text_input("Enter transaction time", "in (HH:MM:SS) format")
-if (tran_time!="in (HH:MM:SS) format"):
-    # Validate time and display error message if format is incorrect
-    if not validate_time_format(tran_time):
-        st.error("format error")
-    # Display the entered time if format is correct
-    else:
-        st.write("You entered:", tran_time)
-merch_id = st.text_input("Enter the merchant's id:")
-cust_id = st.text_input("Enter the customer's id:")
-device_id = st.text_input("Enter the device's id:")
+# tran_date = st.date_input("Select the date of your transaction", datetime.date.today())
+# tran_time = st.text_input("Enter transaction time", "in (HH:MM:SS) format")
+# if (tran_time!="in (HH:MM:SS) format"):
+#     # Validate time and display error message if format is incorrect
+#     if not validate_time_format(tran_time):
+#         st.error("format error")
+#     # Display the entered time if format is correct
+#     else:
+#         st.write("You entered:", tran_time)
+# merch_id = st.text_input("Enter the merchant's id:")
+# cust_id = st.text_input("Enter the customer's id:")
+# device_id = st.text_input("Enter the device's id:")
 trans_type = st.selectbox("Select transaction type", ["Bank Transfer", "Bill Payment", "Investment", "Other", "Purchase", "Refund", "Subscription"])
 pay_type = st.selectbox("Select payment gateway", ["CRED", "Google Pay", "HDFC", "ICICI UPI", "IDFC UPI", "Other", "Paytm", "PhonePe", "Razor Pay"])
 tran_state=st.selectbox("Select transaction state",['Andhra Pradesh', 'Arunachal Pradesh', 'Assam', 'Bihar', 'Chhattisgarh', 'Goa', 'Gujarat', 'Haryana', 'Himachal Pradesh', 'Jharkhand', 'Karnataka', 'Kerala', 'Madhya Pradesh', 'Maharashtra', 'Manipur', 'Meghalaya', 'Mizoram', 'Nagaland', 'Odisha', 'Punjab', 'Rajasthan', 'Sikkim', 'Tamil Nadu', 'Telangana', 'Tripura', 'Uttar Pradesh', 'Uttarakhand', 'West Bengal'])
